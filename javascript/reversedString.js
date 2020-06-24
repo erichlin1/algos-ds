71e7cd6394f300777d87ad7a824159ba370f8b3b
Alyssa Lee <auliee@BoVangs-iMac.local>
2020-06-24 12:49:02 -0700

    solution #2

--- javascript/reversedString.js
+++ javascript/reversedString.js
@@ -1,9 +1,8 @@
 /*
 * solution #1
-*
+* indexes in reverse and concatenates to dummy string.
 */ 
-
-function reversedString(string) {
+function reversedStringOne(string) {
     let reversedString = '';
     const stringLen = string.length - 1;
     for (let index = 0; index <= stringLen; index++) {
@@ -12,7 +11,23 @@ function reversedString(string) {
     return reversedString;
 }
 module.exports = reversedString;
-
+/*
+* solution #2
+* converts string into an array, and performs array methods to get the desired output. 
+*/ 
+function reversedStringTwo(string) {
+    const stringLen = string.length - 1;
+    let stringArr = [];
+    let index = 0;
+    while (index <= stringLen) {
+        /* 
+        * evaluating property expression for string as [index] in stead [stringLen - index]
+        * ... means reverse() would need to be used.
+        */
+        stringArr.push(string[index])
+        index++;
+    };
+}
 
 
 
