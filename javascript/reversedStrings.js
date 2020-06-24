346efdd16affe1eb7597c0e4a8a628de0bd8bf2e
Alyssa Lee <auliee@BoVangs-iMac.local>
2020-06-24 11:15:46 -0700

    reversed strings

---
+++ javascript/reversedStrings.js
@@ -0,0 +1,13 @@
+/*
+* solution #1
+*/ 
+
+const reverseString = (string) => {
+    let reversedString = '';
+    const stringLen = string.length - 1;
+    for (let index = 0; index < stringLen; index++) {
+        
+        
+    }
+
+}
