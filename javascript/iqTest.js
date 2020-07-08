d46fbb6b4eca566183ade23c3ae85e279d47f3cf
EricLin <Ericlin415@gmail.com>
2020-07-08 10:38:55 -0700

    solution #2

--- javascript/iqTest.js
+++ javascript/iqTest.js
@@ -3,21 +3,34 @@
  *
  */
 
-
  /* solution #1 */
+ 
+ function iqTest1(/* str of ints */numbers) {
+    let evenNumArr = [];
+    let oddNumArr = [];
+    const arr = numbers.split(' ');
+    arr.forEach(strInt => {
+        let num = Number(strInt);
+        num % 2 === 0 ? evenNumArr.push(num) : oddNumArr.push(num);
+    });
+    const firstOddVal = arr.indexOf(String(oddNumArr[0])) + 1;
+    const firstEvenVal = arr.indexOf(String(evenNumArr[0])) + 1;
+    return evenNumArr.length !== 1 ? firstOddVal : firstEvenVal;
+}
 
 
 
-function iqTest(/* str of ints */numbers) {
+ /* solution #2 */
+
+function iqTest2(/* str of ints */numbers) {
     let evenNumArr = [];
     let oddNumArr = [];
     let arr = numbers.split(' ');
-    let numbersLen = numbers.length;
-    for (let i = 0; i <= numbersLen - 1; i++) {
+    let arrLen = arr.length;
+    for (let i = 0; i <= arrLen - 1; i++) {
         let num = Number(arr[i]);
-        num % 2 === true ? evenNumArr.push(num) : oddNumArr.push(num);
+        num % 2 === 0 ? evenNumArr.push(num) : oddNumArr.push(num);
     }
-    console.log(evenNumArr);
-    console.log(oddNumArr);
+    return evenNumArr.length !== 1 ? oddNumArr[0] : evenNumArr[0];
+    
 }
-iqTest('2 7 2 2')
