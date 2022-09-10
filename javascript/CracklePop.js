a071d40f09fbfdf41bc286ce8d163455e84d213b
Eric Lin <40250570+ericlinsd@users.noreply.github.com>
2022-09-10 16:25:53 -0700

    Update CracklePop.js

--- javascript/CracklePop.js
+++ javascript/CracklePop.js
@@ -2,6 +2,9 @@
 
 Write a program that prints out the numbers 1 to 100 (inclusive). If the number is divisible by 3, print Crackle instead of the number. If it's divisible by 5, print Pop. If it's divisible by both 3 and 5, print CracklePop. You can use any language.
 
+input: two number args
+output: undefined, console.log values
+conditions: If the value is divisible by 5 and 3 then print 'CracklePop'. If the value is only divisble by 5, then print 'Pop'. If the value is only divisible by 3, then print 'Crackle'. If not divisible either 5 and/or 3, then print the value. 
 */
 
 let isDivisibleByThree = function(num) {
