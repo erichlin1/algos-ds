8fbd3c75f325f677593659f8e4e9344afa5123ea
Eric Lin <40250570+ericlinsd@users.noreply.github.com>
2022-09-29 07:55:42 -0700

    Update pairsMaker.js

--- javascript/pairsMaker.js
+++ javascript/pairsMaker.js
@@ -2,5 +2,36 @@
 
 Write a function pairsMaker(arr) that takes in a an array as an argument. The function should return a 2D array where the subarrays represent unique pairs of element from the input array.
 
+
 */
 
+let pairsMaker = function(arr) {
+    let ans = [];
+    for (let i = 0 ; i < arr.length ; i++ ) {
+        for (let j = i + 1 ; j < arr.length ; j++) {
+            ans.push([arr[i], arr[j]]);
+        };
+    };
+    return ans;
+};
+
+
+console.log(pairsMaker(['a', 'b', 'c', 'd']));
+
+
+
+
+// test cases
+
+console.log(pairsMaker(['a', 'b', 'c', 'd'])); // =>
+// [ [ 'a', 'b' ],
+//   [ 'a', 'c' ],
+//   [ 'a', 'd' ],
+//   [ 'b', 'c' ],
+//   [ 'b', 'd' ],
+//   [ 'c', 'd' ] ]
+
+console.log(pairsMaker(['Rosemary', 'Alex', 'Connor'])); // =>
+// [ [ 'Rosemary', 'Alex' ],
+//   [ 'Rosemary', 'Connor' ],
+//   [ 'Alex', 'Connor' ] ]
