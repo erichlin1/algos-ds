30a8b2cc6b4b288abeb747b0dcd402b0e8afab85
Eric Lin <40250570+ericlinsd@users.noreply.github.com>
2022-09-29 07:45:51 -0700

    Create pairsMaker.js

---
+++ javascript/pairsMaker.js
@@ -0,0 +1,6 @@
+/*
+
+Write a function pairsMaker(arr) that takes in a an array as an argument. The function should return a 2D array where the subarrays represent unique pairs of element from the input array.
+
+*/
+
